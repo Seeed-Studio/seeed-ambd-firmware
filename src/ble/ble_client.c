2bbc7b9f2895ef5cda9f5260b6d8c42cf83952e8
LynnL4 <lht856@foxmail.com>
2020-08-24 11:31:41 +0800

    add ble_client.c for add ble client easily.

---
+++ src/ble/ble_client.c
@@ -0,0 +1,308 @@
+/**
+*****************************************************************************************
+*     Copyright (C) 2020  Seeed Technology Co.,Ltd. All rights reserved.
+*****************************************************************************************
+   * @file      ble_client.c
+   * @brief     
+   * @author    Hongtai.Liu
+   * @date      2020-08-21
+   * @version   v1.0
+   **************************************************************************************
+   * @attention
+   * <h2><center>&copy; COPYRIGHT 2020 Seeed Technology Co.,Ltd</center></h2>
+   **************************************************************************************
+  */
+
+/** Add Includes here **/
+#include <bt_flags.h>
+#include <trace_app.h>
+#include <string.h>
+#include <os_mem.h>
+#include <ble_client.h>
+
+/********************************************************************************************************
+* local static variables defined here, only used in this source file.
+********************************************************************************************************/
+/** @brief  GAPS Client link table */
+static uint8_t ble_client_link_num[BLE_CLIENT_MAX_APPS];
+/**<  GATTS client ID. */
+static T_CLIENT_ID ble_client_id[BLE_CLIENT_MAX_APPS];
+/**<  Callback used to send data to app from ble_client client layer. */
+static P_FUN_GENERAL_APP_CB ble_client_client_cb = NULL;
+
+static void ble_client_discover_state_cb0(uint8_t conn_id, T_DISCOVERY_STATE discovery_state)
+{
+    T_BLE_CLIENT_CB_DATA cb_data;
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_DISCOVERY_STATE;
+    cb_data.app_id = 0;
+    cb_data.cb_content.discov_state.state = discovery_state;
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[0], conn_id, &cb_data);
+    }
+    return;
+}
+
+static void ble_client_discover_result_cb0(uint8_t conn_id, T_DISCOVERY_RESULT_TYPE result_type,
+                                           T_DISCOVERY_RESULT_DATA result_data)
+{
+    T_BLE_CLIENT_CB_DATA cb_data;
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_DISCOVERY_RESULT;
+    cb_data.app_id = 0;
+    cb_data.cb_content.discov_result.discov_type = result_type;
+    memcpy(&(cb_data.cb_content.discov_result.result), result_data.p_srv_disc_data, sizeof(T_BLE_CLIENT_DISCOV_RESULT_DATA));
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[0], conn_id, &cb_data);
+    }
+    return;
+}
+
+static void ble_client_read_result_cb0(uint8_t conn_id, uint16_t cause,
+                                       uint16_t handle, uint16_t value_size, uint8_t *p_value)
+{
+    T_BLE_CLIENT_CB_DATA cb_data;
+    log_i("gcs_client_read_result_cb0: handle 0x%x, cause 0x%x", handle, cause);
+
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_READ_RESULT;
+    cb_data.app_id = 0;
+    cb_data.cb_content.read_result.cause = cause;
+    cb_data.cb_content.read_result.handle = handle;
+    cb_data.cb_content.read_result.value_size = value_size;
+    cb_data.cb_content.read_result.p_value = p_value;
+
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[0], conn_id, &cb_data);
+    }
+    return;
+}
+
+static void ble_client_write_result_cb0(uint8_t conn_id, T_GATT_WRITE_TYPE type,
+                                        uint16_t handle, uint16_t cause,
+                                        uint8_t credits)
+{
+    (void)credits;
+    T_BLE_CLIENT_CB_DATA cb_data;
+    log_i("ble_client_write_result_cb0: handle 0x%x, cause 0x%x", handle, cause);
+
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_WRITE_RESULT;
+    cb_data.app_id = 0;
+    cb_data.cb_content.write_result.cause = cause;
+    cb_data.cb_content.write_result.handle = handle;
+    cb_data.cb_content.write_result.type = type;
+
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[0], conn_id, &cb_data);
+    }
+    return;
+}
+
+static T_APP_RESULT ble_client_notify_ind_cb0(uint8_t conn_id, bool notify,
+                                              uint16_t handle,
+                                              uint16_t value_size, uint8_t *p_value)
+{
+    T_BLE_CLIENT_CB_DATA cb_data;
+    T_APP_RESULT app_result = APP_RESULT_SUCCESS;
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_NOTIF_IND;
+    cb_data.app_id = 0;
+    cb_data.cb_content.notif_ind.notify = notify;
+    cb_data.cb_content.notif_ind.handle = handle;
+    cb_data.cb_content.notif_ind.value_size = value_size;
+    cb_data.cb_content.notif_ind.p_value = p_value;
+
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[0], conn_id, &cb_data);
+    }
+    return app_result;
+}
+
+static void ble_client_disc_cb0(uint8_t conn_id)
+{
+    T_BLE_CLIENT_CB_DATA cb_data;
+
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_DISCONNECT_RESULT;
+    cb_data.app_id = 0;
+    cb_data.cb_content.disconn_result.conn_id = conn_id;
+
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[0], conn_id, &cb_data);
+    }
+
+    return;
+}
+
+static void ble_client_discover_state_cb1(uint8_t conn_id, T_DISCOVERY_STATE discovery_state)
+{
+    T_BLE_CLIENT_CB_DATA cb_data;
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_DISCOVERY_STATE;
+    cb_data.app_id = 1;
+    cb_data.cb_content.discov_state.state = discovery_state;
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[1], conn_id, &cb_data);
+    }
+    return;
+}
+
+static void ble_client_discover_result_cb1(uint8_t conn_id, T_DISCOVERY_RESULT_TYPE result_type,
+                                           T_DISCOVERY_RESULT_DATA result_data)
+{
+    T_BLE_CLIENT_CB_DATA cb_data;
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_DISCOVERY_RESULT;
+    cb_data.app_id = 1;
+    cb_data.cb_content.discov_result.discov_type = result_type;
+    memcpy(&(cb_data.cb_content.discov_result.result), result_data.p_srv_disc_data, sizeof(T_BLE_CLIENT_DISCOV_RESULT_DATA));
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[0], conn_id, &cb_data);
+    }
+    return;
+}
+
+static void ble_client_read_result_cb1(uint8_t conn_id, uint16_t cause,
+                                       uint16_t handle, uint16_t value_size, uint8_t *p_value)
+{
+    T_BLE_CLIENT_CB_DATA cb_data;
+    log_i("gcs_client_read_result_cb1: handle 0x%x, cause 0x%x", handle, cause);
+
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_READ_RESULT;
+    cb_data.app_id = 1;
+    cb_data.cb_content.read_result.cause = cause;
+    cb_data.cb_content.read_result.handle = handle;
+    cb_data.cb_content.read_result.value_size = value_size;
+    cb_data.cb_content.read_result.p_value = p_value;
+
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[1], conn_id, &cb_data);
+    }
+    return;
+}
+
+static void ble_client_write_result_cb1(uint8_t conn_id, T_GATT_WRITE_TYPE type,
+                                        uint16_t handle, uint16_t cause,
+                                        uint8_t credits)
+{
+    (void)credits;
+    T_BLE_CLIENT_CB_DATA cb_data;
+    log_i("ble_client_write_result_cb1: handle 0x%x, cause 0x%x", handle, cause);
+
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_WRITE_RESULT;
+    cb_data.app_id = 1;
+    cb_data.cb_content.write_result.cause = cause;
+    cb_data.cb_content.write_result.handle = handle;
+    cb_data.cb_content.write_result.type = type;
+
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[1], conn_id, &cb_data);
+    }
+    return;
+}
+
+static T_APP_RESULT ble_client_notify_ind_cb1(uint8_t conn_id, bool notify,
+                                              uint16_t handle,
+                                              uint16_t value_size, uint8_t *p_value)
+{
+    T_BLE_CLIENT_CB_DATA cb_data;
+    T_APP_RESULT app_result = APP_RESULT_SUCCESS;
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_NOTIF_IND;
+    cb_data.app_id = 1;
+    cb_data.cb_content.notif_ind.notify = notify;
+    cb_data.cb_content.notif_ind.handle = handle;
+    cb_data.cb_content.notif_ind.value_size = value_size;
+    cb_data.cb_content.notif_ind.p_value = p_value;
+
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[1], conn_id, &cb_data);
+    }
+    return app_result;
+}
+
+static void ble_client_disc_cb1(uint8_t conn_id)
+{
+    T_BLE_CLIENT_CB_DATA cb_data;
+
+    cb_data.cb_type = BLE_CLIENT_CB_TYPE_DISCONNECT_RESULT;
+    cb_data.app_id = 1;
+    cb_data.cb_content.disconn_result.conn_id = conn_id;
+
+    if (ble_client_client_cb)
+    {
+        (*ble_client_client_cb)(ble_client_id[1], conn_id, &cb_data);
+    }
+
+    return;
+}
+
+
+/**
+ * @brief GATTS Client Callbacks.
+*/
+const T_FUN_CLIENT_CBS ble_client_cbs[BLE_CLIENT_MAX_APPS] = {
+    {
+        ble_client_discover_state_cb0,  //!< Discovery State callback function pointer
+        ble_client_discover_result_cb0, //!< Discovery result callback function pointer
+        ble_client_read_result_cb0,     //!< Read response callback function pointer
+        ble_client_write_result_cb0,    //!< Write result callback function pointer
+        ble_client_notify_ind_cb0,      //!< Notify Indicate callback function pointer
+        ble_client_disc_cb0             //!< Link disconnection callback function pointer
+    },
+    {
+        ble_client_discover_state_cb1,  //!< Discovery State callback function pointer
+        ble_client_discover_result_cb1, //!< Discovery result callback function pointer
+        ble_client_read_result_cb1,     //!< Read response callback function pointer
+        ble_client_write_result_cb1,    //!< Write result callback function pointer
+        ble_client_notify_ind_cb1,      //!< Notify Indicate callback function pointer
+        ble_client_disc_cb1             //!< Link disconnection callback function pointer
+    }};
+
+bool ble_client_init(uint8_t num)
+{
+    if (num > BLE_CLIENT_MAX_APPS)
+    {
+        return false;
+    }
+    for (uint8_t i = 0; i < BLE_CLIENT_MAX_APPS; i++)
+    {
+        ble_client_id[i] = CLIENT_PROFILE_GENERAL_ID;
+        ble_client_link_num[i] = 0;
+    }
+
+    client_init(num);
+
+    return true;
+}
+
+T_CLIENT_ID ble_add_client(uint8_t app_id, P_FUN_GENERAL_APP_CB app_cb, uint8_t link_num,
+                           uint16_t max_discov_table_size)
+{
+    uint8_t i;
+    uint16_t size;
+    if (link_num > BLE_CLIENT_MAX_LINKS)
+    {
+        return 0xff;
+    }
+    if (app_id > BLE_CLIENT_MAX_APPS)
+    {
+        return 0xff;
+    }
+
+    ble_client_client_cb = app_cb;
+    ble_client_link_num[app_id] = link_num;
+
+    if (false == client_register_spec_client_cb(&ble_client_id, &ble_client_cbs[app_id]))
+    {
+        ble_client_id[app_id] = CLIENT_PROFILE_GENERAL_ID;
+        log_e("ble_client_add_client: register fail");
+        return ble_client_id[app_id];
+    }
+
+    log_i("ble_client_add_client: client id %d", ble_client_id);
+    return ble_client_id[app_id];
+}
---
+++ src/ble/ble_client.h
@@ -0,0 +1,139 @@
+/**
+*****************************************************************************************
+*     Copyright (C) 2020  Seeed Technology Co.,Ltd. All rights reserved.
+*****************************************************************************************
+   * @file      ble_client.h
+   * @brief     
+   * @author    Hongtai.Liu
+   * @date      2020-08-21
+   * @version   v1.0
+   **************************************************************************************
+   * @attention
+   * <h2><center>&copy; COPYRIGHT 2020 Seeed Technology Co.,Ltd</center></h2>
+   **************************************************************************************
+  */
+#ifndef _BLE_CLIENT_H_
+#define _BLE_CLIENT_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif /* __cplusplus */
+
+/* Add Includes here */
+#include <bt_flags.h>
+#include <profile_client.h>
+#include "elog.h"
+
+#define BLE_CLIENT_MAX_APPS 2
+#define BLE_CLIENT_MAX_LINKS 4
+
+  /** @brief  Discovery result type.*/
+  typedef enum
+  {
+    BLE_CLIENT_ALL_PRIMARY_SRV_DISCOV, //!<Response type for BLE_CLIENT_all_primary_srv_discovery
+    BLE_CLIENT_BY_UUID128_SRV_DISCOV,  //!<Response type for BLE_CLIENT_by_uuid128_srv_discovery
+    BLE_CLIENT_BY_UUID_SRV_DISCOV,     //!<Response type for BLE_CLIENT_by_uuid_srv_discovery
+    BLE_CLIENT_ALL_CHAR_DISCOV,        //!<Response type for BLE_CLIENT_all_char_discovery
+    BLE_CLIENT_BY_UUID_CHAR_DISCOV,    //!<Response type for BLE_CLIENT_by_uuid_char_discovery
+    BLE_CLIENT_BY_UUID128_CHAR_DISCOV, //!<Response type for BLE_CLIENT_by_uuid128_char_discovery
+    BLE_CLIENT_ALL_CHAR_DESC_DISCOV,   //!<Response type for BLE_CLIENT_all_char_descriptor_discovery
+  } T_BLE_CLIENT_DISCOV_TYPE;
+
+  /** @brief  Discovery result element.*/
+  typedef union {
+    T_GATT_SERVICE_ELEM16 srv_uuid16_disc_data;
+    T_GATT_SERVICE_ELEM128 srv_uuid128_disc_data;
+    T_GATT_SERVICE_BY_UUID_ELEM srv_disc_data;
+    T_GATT_CHARACT_ELEM16 char_uuid16_disc_data;
+    T_GATT_CHARACT_ELEM128 char_uuid128_disc_data;
+    T_GATT_CHARACT_DESC_ELEM16 char_desc_uuid16_disc_data;
+    T_GATT_CHARACT_DESC_ELEM128 char_desc_uuid128_disc_data;
+  } T_BLE_CLIENT_DISCOV_RESULT_DATA;
+
+  /** @brief BLE client callback type */
+  typedef enum
+  {
+    BLE_CLIENT_CB_TYPE_DISCOVERY_STATE, //!< Discovery procedure state, done or pending.
+    BLE_CLIENT_CB_TYPE_DISCOVERY_RESULT, //!< Discovery procedure state, done or pending.
+    BLE_CLIENT_CB_TYPE_READ_RESULT, //!< Read request's result data, responsed from server.
+    BLE_CLIENT_CB_TYPE_WRITE_RESULT,
+    BLE_CLIENT_CB_TYPE_NOTIF_IND,
+    BLE_CLIENT_CB_TYPE_DISCONNECT_RESULT, //!< Invalid callback type, no practical usage.
+    BLE_CLIENT_CB_TYPE_INVALID            //!< Invalid callback type, no practical usage.
+  } T_BLE_CLIENT_CB_TYPE;
+
+  /** @brief BLE client discovery result */
+
+   typedef struct
+  {
+    T_DISCOVERY_STATE state;
+  } T_BLE_CLIENT_DISCOVERY_STATE;
+
+  typedef struct
+  {
+    T_BLE_CLIENT_DISCOV_TYPE discov_type;
+    T_BLE_CLIENT_DISCOV_RESULT_DATA result;
+  } T_BLE_CLIENT_DISCOVERY_RESULT;
+
+   /** @brief BLE client discovery result */
+  typedef struct
+  {
+    uint8_t conn_id;
+  } T_BLE_CLIENT_DISCONNECT_RESULT;
+
+
+  /** @brief BLE client read data, used to inform app read response data */
+  typedef struct
+  {
+    uint16_t cause;
+    uint16_t handle;
+    uint16_t value_size;
+    uint8_t *p_value;
+  } T_BLE_CLIENT_READ_RESULT;
+
+  /** @brief BLE client write result, used to inform app write response data */
+  typedef struct
+  {
+    uint16_t cause;
+    uint16_t handle;
+    T_GATT_WRITE_TYPE type;
+  } T_BLE_CLIENT_WRITE_RESULT;
+
+  /** @brief BLE client notification/indication*/
+  typedef struct
+  {
+    bool notify;
+    uint16_t handle;
+    uint16_t value_size;
+    uint8_t *p_value;
+  } T_BLE_CLIENT_NOTIF_IND;
+
+  /** @brief BLE client callback content */
+  typedef union {
+    T_BLE_CLIENT_DISCOVERY_STATE discov_state;
+    T_BLE_CLIENT_DISCOVERY_RESULT discov_result;
+    T_BLE_CLIENT_READ_RESULT read_result;
+    T_BLE_CLIENT_WRITE_RESULT write_result;
+    T_BLE_CLIENT_NOTIF_IND notif_ind;
+    T_BLE_CLIENT_DISCONNECT_RESULT disconn_result;
+  } T_BLE_CLIENT_CB_CONTENT;
+
+  /** @brief BLE client callback data */
+  typedef struct
+  {
+    uint8_t app_id;
+    T_BLE_CLIENT_CB_TYPE cb_type;
+    T_BLE_CLIENT_CB_CONTENT cb_content;
+  } T_BLE_CLIENT_CB_DATA;
+
+bool ble_client_init(uint8_t num);
+T_CLIENT_ID ble_add_client(uint8_t app_id, P_FUN_GENERAL_APP_CB app_cb, uint8_t link_num,
+                           uint16_t max_discov_table_size);
+
+#ifdef __cplusplus
+  extern "C"
+}
+#endif
+
+#endif
