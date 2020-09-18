30cd48b75ce679b87271c0c0480af8c4d5037778
LynnL4 <lht856@foxmail.com>
2020-09-18 11:19:51 +0800

    add ps1 for windows project.

---
+++ build.ps1
@@ -0,0 +1,54 @@
+param($1,$2)
+$PROJECT="seeed-ambd-firmware"
+$BOARD="realtek:AmebaD:ameba_rtl8721d"
+$COMMAND=$1 
+$PORT=$2
+$COMMAND
+$CRTDIR=Split-Path -Parent $MyInvocation.MyCommand.Definition
+
+#easylogger
+$INCLUDE+=" -I$CRTDIR/src/easylogger"
+$INCLUDE+=" -I$CRTDIR/src/easylogger/inc"
+
+#ble
+$INCLUDE+=" -I$CRTDIR/src/ble"
+$INCLUDE+=" -I$CRTDIR/src/ble_central"
+
+#esp_lib
+$INCLUDE+=" -I$CRTDIR/src/esp_lib"
+
+#erpc
+$INCLUDE+=" -I$CRTDIR/src/erpc"
+
+#erpc_shim
+$INCLUDE+=" -I$CRTDIR/src/erpc_shim"
+
+$FLAGS+=""
+
+
+if ("$COMMAND" -eq "--build"){
+	Write-Output "Building $PROJECT"
+	arduino-cli compile --fqbn  $BOARD --verbose --build-properties build.extra_flags="$INCLUDE $FLAGS" $PROJECT 
+}
+elseif ( "$COMMAND" -eq "--flash" ){
+	if(!$PORT)
+	{
+	 Write-Output "port not allow null"
+	 exit
+	}
+	arduino-cli upload  --fqbn $BOARD -p $PORT
+}
+elseif ("$COMMAND" -eq "--all" )
+{
+	if(!$PORT)
+	{
+	 Write-Output "port not allow null"
+	 exit
+	}
+	arduino-cli compile --fqbn  $BOARD --verbose --build-properties build.extra_flags="$INCLUDE $FLAGS" $PROJECT
+	arduino-cli upload  --fqbn $BOARD -p $PORT
+}
+else 
+{
+	Write-Output "Nothing to do for target"
+}
---
+++ gen_ble.ps1
@@ -0,0 +1,12 @@
+#!/bin/bash
+Write-Output "eneter erpc_idl"
+Set-Location erpc_idl
+Write-Output "gen erpc_shim"
+erpcgen rpc_ble.erpc
+Set-Location ..
+Write-Output "auto release"
+Remove-Item src/erpc_shim/* 
+Copy-Item erpc_idl/erpc_shim/* src/erpc_shim/
+Remove-Item src/erpc_shim/rpc_ble_api_client.cpp
+Remove-Item src/erpc_shim/rpc_ble_callback_server.cpp
+Remove-Item src/erpc_shim/rpc_ble_callback_server.h
