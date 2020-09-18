5ade01f8d1edfc268c6cb27214ad54cb5f88a7b3
LynnL4 <lht856@foxmail.com>
2020-09-18 15:44:50 +0800

    Add the RPC API for WiFi related functions, and temporarily make it work with ble

--- gen_ble.ps1
+++ gen_ble.ps1
@@ -1,12 +1,14 @@
 #!/bin/bash
 Write-Output "eneter erpc_idl"
 Set-Location erpc_idl
-Write-Output "gen erpc_shim"
-erpcgen rpc_ble.erpc
+Write-Output "gen erpc_shim ble"
+erpcgen rpc_system.erpc
 Set-Location ..
 Write-Output "auto release"
 Remove-Item src/erpc_shim/* 
 Copy-Item erpc_idl/erpc_shim/* src/erpc_shim/
+
 Remove-Item src/erpc_shim/rpc_ble_api_client.cpp
 Remove-Item src/erpc_shim/rpc_ble_callback_server.cpp
 Remove-Item src/erpc_shim/rpc_ble_callback_server.h
+Remove-Item src/erpc_shim/rpc_wifi_api_client.cpp
--- gen_ble.sh
+++ gen_ble.sh
@@ -6,7 +6,7 @@ erpcgen rpc_ble.erpc
 cd ..
 echo "auto release"
 rm src/erpc_shim/* -rf
-cp erpc_idl/erpc_shim/* src/erpc_shim/
-rm src/erpc_shim/rpc_ble_api_client.cpp
-rm src/erpc_shim/rpc_ble_callback_server.cpp
-rm src/erpc_shim/rpc_ble_callback_server.h
+cp erpc_idl/erpc_shim/ble/* src/erpc_shim/
+rm src/erpc_shim/ble/rpc_ble_api_client.cpp
+rm src/erpc_shim/ble/rpc_ble_callback_server.cpp
+rm src/erpc_shim/ble/rpc_ble_callback_server.h
