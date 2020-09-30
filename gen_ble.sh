8707cde711866440e61392897f7defc07b7becc0
LynnL4 <lht856@foxmail.com>
2020-09-30 14:43:32 +0800

    update erpc shim

--- gen_ble.sh
+++ gen_ble.sh
@@ -2,11 +2,14 @@
 echo "eneter erpc_idl"
 cd erpc_idl
 echo "gen erpc_shim"
-erpcgen rpc_ble.erpc
+erpcgen rpc_system.erpc
 cd ..
 echo "auto release"
 rm src/erpc_shim/* -rf
-cp erpc_idl/erpc_shim/ble/* src/erpc_shim/
-rm src/erpc_shim/ble/rpc_ble_api_client.cpp
-rm src/erpc_shim/ble/rpc_ble_callback_server.cpp
-rm src/erpc_shim/ble/rpc_ble_callback_server.h
+
+cp erpc_idl/erpc_shim/*  src/erpc_shim/
+rm src/erpc_shim/rpc_ble_api_client.cpp
+rm src/erpc_shim/rpc_ble_callback_server.cpp
+rm src/erpc_shim/rpc_ble_callback_server.h
+rm src/erpc_shim/rpc_wifi_api_client.cpp
+
