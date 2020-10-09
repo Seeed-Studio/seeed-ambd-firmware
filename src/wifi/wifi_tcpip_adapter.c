afa519cbf3a9a6f5d9cb9534f27f32046b798282
LynnL4 <lht856@foxmail.com>
2020-10-09 17:13:13 +0800

    add tcpip adapter functions.

---
+++ src/wifi/wifi_tcpip_adapter.c
@@ -0,0 +1,251 @@
+
+// Copyright 2015-2016 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/**
+*****************************************************************************************
+*     Copyright (C) 2020  Seeed Technology Co.,Ltd. All rights reserved.
+*****************************************************************************************
+   * @file      wifi_tcpip_adapter.c
+   * @brief     
+   * @author    Hongtai.Liu
+   * @date      2020-10-9
+   * @version   v1.0
+   **************************************************************************************
+   * @attention
+   * <h2><center>&copy; COPYRIGHT 2020 Seeed Technology Co.,Ltd</center></h2>
+   **************************************************************************************
+  */
+
+#include "main.h"
+#include "wifi_tcpip_adapter.h"
+
+extern struct netif xnetif[NET_IF_NUM];
+
+void tcpip_adapter_init(void)
+{
+    LwIP_init();
+}
+
+esp_err_t tcpip_adapter_eth_start(uint8_t *mac, tcpip_adapter_ip_info_t *ip_info)
+{
+    return ESP_OK;
+}
+
+esp_err_t tcpip_adapter_sta_start(uint8_t *mac, tcpip_adapter_ip_info_t *ip_info)
+{
+    esp_err_t ret = ESP_OK;
+    uint8_t dhcp_result;
+
+    if (RTW_SUCCESS != wifi_set_mac_address(mac))
+    {
+        ret = ESP_ERR_TCPIP_ADAPTER_INVALID_PARAMS;
+        goto exit;
+    }
+
+    dhcp_result = LwIP_DHCP(0, DHCP_START);
+
+    if (dhcp_result != DHCP_ADDRESS_ASSIGNED)
+    {
+        ret = ESP_ERR_TCPIP_ADAPTER_DHCPC_START_FAILED;
+        goto exit;
+    }
+
+exit:
+    return ret;
+}
+
+esp_err_t tcpip_adapter_ap_start(uint8_t *mac, tcpip_adapter_ip_info_t *ip_info)
+{
+    esp_err_t ret = ESP_OK;
+
+    if (RTW_SUCCESS != wifi_set_mac_address(mac))
+    {
+        ret = ESP_ERR_TCPIP_ADAPTER_IF_NOT_READY;
+    }
+
+    uint8_t dhcp_result;
+    struct netif *pnetif = &xnetif[0];
+
+    dhcps_deinit();
+    wifi_off();
+    vTaskDelay(20);
+    if (wifi_on(RTW_MODE_AP) < 0)
+    {
+        ret = ESP_ERR_TCPIP_ADAPTER_IF_NOT_READY;
+        goto exit;
+    }
+
+    netif_set_addr(pnetif, &ip_info->ip, &ip_info->netmask, &ip_info->gw);
+
+    dhcps_init(pnetif);
+
+exit:
+    return ret;
+}
+
+esp_err_t tcpip_adapter_stop(tcpip_adapter_if_t tcpip_if)
+{
+    esp_err_t ret = ESP_OK;
+    uint8_t dhcp_result = LwIP_DHCP(tcpip_if, DHCP_STOP);
+    if (DHCP_STOP != dhcp_result)
+        ;
+    return ESP_FAIL;
+    return ret;
+}
+
+esp_err_t tcpip_adapter_up(tcpip_adapter_if_t tcpip_if)
+{
+    esp_err_t ret = ESP_OK;
+   struct netif *pnetif = &xnetif[tcpip_if];
+    if (tcpip_if <= NET_IF_NUM)
+    {
+        dhcps_init(pnetif);
+    }
+    else
+    {
+        return ESP_ERR_TCPIP_ADAPTER_IF_NOT_READY;
+    }
+
+    return ret;
+}
+
+esp_err_t tcpip_adapter_down(tcpip_adapter_if_t tcpip_if)
+{
+    esp_err_t ret = ESP_OK;
+    dhcps_deinit();
+    wifi_off();
+    return ret;
+}
+
+esp_err_t tcpip_adapter_get_ip_info(tcpip_adapter_if_t tcpip_if, tcpip_adapter_ip_info_t *ip_info)
+{
+    esp_err_t ret = ESP_OK;
+    struct netif *pnetif = &xnetif[tcpip_if];
+    memcpy(&ip_info->ip, &pnetif->ip_addr, sizeof(ip_addr_t));
+    memcpy(&ip_info->netmask, &pnetif->netmask, sizeof(ip_addr_t));
+    memcpy(&ip_info->gw, &pnetif->gw, sizeof(ip_addr_t));
+
+    return ret;
+}
+
+esp_err_t tcpip_adapter_set_ip_info(tcpip_adapter_if_t tcpip_if, tcpip_adapter_ip_info_t *ip_info)
+{
+    esp_err_t ret = ESP_OK;
+    struct netif *pnetif = &xnetif[tcpip_if];
+
+    memcpy(&pnetif->ip_addr, &ip_info->ip, sizeof(ip_addr_t));
+    memcpy(&pnetif->netmask, &ip_info->netmask, sizeof(ip_addr_t));
+    memcpy(&pnetif->gw, &ip_info->gw, sizeof(ip_addr_t));
+
+    return ret;
+}
+esp_err_t tcpip_adapter_set_dns_info(tcpip_adapter_if_t tcpip_if, tcpip_adapter_dns_type_t type, tcpip_adapter_dns_info_t *dns)
+{
+    esp_err_t ret = ESP_OK;
+
+    dns_setserver(0, &dns->ip);
+
+    return ret;
+}
+
+esp_err_t tcpip_adapter_get_dns_info(tcpip_adapter_if_t tcpip_if, tcpip_adapter_dns_type_t type, tcpip_adapter_dns_info_t *dns)
+{
+    esp_err_t ret = ESP_OK;
+
+    ip_addr_t *ip = dns_getserver(0);
+
+    memcpy(&dns->ip, ip, sizeof(ip_addr_t));
+
+    return ret;
+}
+
+esp_err_t tcpip_adapter_dhcps_start(tcpip_adapter_if_t tcpip_if)
+{
+    esp_err_t ret = ESP_OK;
+    uint8_t dhcp_result;
+    dhcp_result = LwIP_DHCP(tcpip_if, DHCP_START);
+
+    if (dhcp_result != DHCP_ADDRESS_ASSIGNED)
+    {
+        ret = ESP_FAIL;
+    }
+
+    return ret;
+}
+
+esp_err_t tcpip_adapter_dhcps_stop(tcpip_adapter_if_t tcpip_if)
+{
+    esp_err_t ret = ESP_OK;
+    uint8_t dhcp_result;
+    dhcp_result = LwIP_DHCP(tcpip_if, DHCP_STOP);
+
+    if (dhcp_result != DHCP_STOP)
+    {
+        ret = ESP_FAIL;
+    }
+
+    return ret;
+}
+
+esp_err_t tcpip_adapter_dhcpc_start(tcpip_adapter_if_t tcpip_if)
+{
+    esp_err_t ret = ESP_OK;
+    uint8_t dhcp_result;
+    dhcp_result = LwIP_DHCP(tcpip_if, DHCP_START);
+
+    if (dhcp_result != DHCP_ADDRESS_ASSIGNED)
+    {
+        ret = ESP_FAIL;
+    }
+
+    return ret;
+}
+
+esp_err_t tcpip_adapter_dhcpc_stop(tcpip_adapter_if_t tcpip_if)
+{
+    esp_err_t ret = ESP_OK;
+    uint8_t dhcp_result;
+    dhcp_result = LwIP_DHCP(tcpip_if, DHCP_STOP);
+
+    if (dhcp_result != DHCP_STOP)
+    {
+        ret = ESP_FAIL;
+    }
+
+    return ret;
+}
+
+esp_err_t tcpip_adapter_set_hostname(tcpip_adapter_if_t tcpip_if, const char *hostname)
+{
+    netif_set_hostname(&xnetif[tcpip_if], hostname);
+    return ESP_OK;
+}
+
+esp_err_t tcpip_adapter_get_hostname(tcpip_adapter_if_t tcpip_if, const char **hostname)
+{
+    *hostname = netif_get_hostname(&xnetif[tcpip_if]);
+    return ESP_OK;
+}
+
+esp_err_t tcpip_adapter_get_mac(tcpip_adapter_if_t tcpip_if, uint8_t *mac)
+{
+    wifi_get_mac_address(mac);
+     return ESP_OK;
+}
+
+esp_err_t tcpip_adapter_set_mac(tcpip_adapter_if_t tcpip_if, uint8_t *mac)
+{
+    wifi_set_mac_address(mac);
+    return ESP_OK;
+}
