7235ec0e2a2cf011fcbe9c03626dd8cb1d196e07
LynnL4 <lht856@foxmail.com>
2020-10-10 10:28:31 +0800

    add wifi tcpip rpc apis.

--- src/erpc_setup.cpp
+++ src/erpc_setup.cpp
@@ -93,6 +93,7 @@ void add_services(erpc::SimpleServer *server)
     server->addService(static_cast<erpc::Service *>(create_rpc_gatt_client_service()));
     server->addService(static_cast<erpc::Service *>(create_rpc_gatt_server_service()));
     server->addService(static_cast<erpc::Service *>(create_rpc_wifi_drv_service()));
+    server->addService(static_cast<erpc::Service *>(create_rpc_wifi_tcpip_service()));
 }
 
 /**
